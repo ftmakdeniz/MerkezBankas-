821cd5ea32681dc80d0e36b1ff66fb67ca18ce0e
ftm <99824014+ftmakdeniz@users.noreply.github.com>
2023-04-02 00:03:30 +0300

    Add files via upload
    Merkez Bankasından Rest API ile Döviz Kurlarını Çekme

---
+++ MerkezBankasindan/Models.cs
@@ -0,0 +1,27 @@
+﻿namespace MerkezBankasindan
+{
+    public class RequestData
+    {
+        public int Gun { get; set; }
+        public int Ay { get; set; }      
+        public int Yil { get; set; } 
+        public bool IsBugun { get; set; } 
+    }
+    public class ResponseDataKur
+    {
+        public string Kodu { get; set; }
+        public string Adi { get; set; }
+        public int Birimi { get; set; }
+        public decimal AlisKuru { get; set; }
+        public decimal SatisKuru { get; set; }
+        public decimal EfektifAlisKuru { get; set; }
+        public decimal efektifSatiskuru { get; set; }
+    }
+    public class ResponseData
+    {
+        public List<ResponseDataKur> Data { get; set; }
+        public string Hata { get; set; }
+    }
+}
+
+//f.a
